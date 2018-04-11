00eccadbc255548d49e421c094e74381205daa06
Sand Sand <saibaoomfu@gmail.com>
2018-04-11 13:06:11 +0200

    Initial commit

---
+++ resources/views/user/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    @include('user/layouts/head')
+  </head>
+
+  <body>
+   @include('user/layouts/header')
+    <!-- Main Content -->
+    @section('main-content')
+    @show
+
+    <!-- Footer -->
+    @include('user/layouts/footer')
+
+  </body>
+
+</html>
